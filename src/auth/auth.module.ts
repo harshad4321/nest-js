b0448a314232645d94674202ad2f47a9ec4c58e7
Harshad.N <harshad.n4321@gmail.com>
2023-04-03 13:39:51 +0530

    set

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AutController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AutController],
   providers: [AuthService],
 })
