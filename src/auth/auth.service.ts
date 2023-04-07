45a57236d86543be3e233469d6882f0643b80587
Harshad.N <harshad.n4321@gmail.com>
2023-04-07 23:53:35 +0530

    is set

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,53 @@
-import { Injectable, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup() {
-    return { message: 'I have  signed up' };
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: { email: dto.email, hash },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new BadRequestException(
+          'Email address is already in use',
+        );
+      }
+      throw error;
+    }
   }
-  signin() {
-    return { message: 'I have  signed in' };
+
+  async signin(dto: AuthDto) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: { email: dto.email },
+      });
+    if (!user) {
+      throw new BadRequestException(
+        'Incorrect credentials',
+      );
+    }
+    const pwMatches = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+    if (!pwMatches) {
+      throw new BadRequestException(
+        'Incorrect credentials',
+      );
+    }
+    delete user.hash;
+    return user;
   }
 }
