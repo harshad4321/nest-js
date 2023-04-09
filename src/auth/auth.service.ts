342ba6b7e6e13db1ab038b8f7bca4f92c6f7a1f6
Harshad.N <harshad.n4321@gmail.com>
2023-04-09 23:08:31 +0530

    set

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,5 @@
+import { IsString } from 'class-validator';
+import { JwtService } from '@nestjs/jwt';
 import {
   BadRequestException,
   Injectable,
@@ -5,20 +7,30 @@ import {
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { Config } from 'prettier';
+import { ConfigService } from '@nestjs/config';
+import { promises } from 'dns';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private Jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signup(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
     //for the password hashing   -> argon2 isused(in bcrypt is ony possible to hash first 72byte so)
     try {
       const user = await this.prisma.user.create({
-        data: { email: dto.email, hash },
+        data: {
+          email: dto.email,
+          hash,
+        },
       });
-      delete user.hash;
-      return user;
+
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error.code === 'P2002') {
         throw new BadRequestException(
@@ -48,7 +60,22 @@ export class AuthService {
         'Incorrect credentials',
       );
     }
-    delete user.hash;
-    return user;
+    return this.signToken(user.id, user.email);
+  }
+  signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+    return this.Jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret,
+    }).then(async (token) => ({
+      access_token: token,
+    }));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable validation using the ValidationPipe middleware
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true }),
+  );
   await app.listen(3000);
 }
 bootstrap();
